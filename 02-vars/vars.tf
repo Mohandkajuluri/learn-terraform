fc29db6b4c8b87dc227217b6b9638f28ea3edc09
Mohandkajuluri <md.kajuluri@gmail.com>
2023-12-16 10:29:44 -0500

    TF types of vars

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -2,11 +2,11 @@ variable "sample" {
   default = "hello"
 }
  output "sample1" {
-   value = "var.sample1"
+   value = var.sample1
  }
 variable "fruits" {
   default = ["apple", "banana", "mango"]
 }
 output "fruit_first_fruit" {
-  value = "var.fruits"
+  value = var.fruits
 }
